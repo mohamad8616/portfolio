54b2d569450094f60e38eaafc5bd5186056ca82e
mohamad8616 <mohamad.sh.a9036@gmail.com>
2025-04-30 18:37:34 +0330

    returned next config

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
